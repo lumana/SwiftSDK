d20f6e9d2ea6d6461a0c168ef663b6c9ec34f6db
Luis Umaña <lumanaucr@hotmail.com>
2023-06-05 09:32:21 -0600

    First Commit. Create webView for test SDK

--- Sources/SwiftSDK/SwiftSDK.swift
+++ Sources/SwiftSDK/SwiftSDK.swift
@@ -1,6 +1,27 @@
-public struct SwiftSDK {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import UIKit
+import WebKit
 
+public class SwiftSDK: WKWebView {
+    
     public init() {
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.preferences.javaScriptEnabled = true
+        super.init(frame: .zero, configuration: webConfiguration)
+        self.scrollView.isScrollEnabled = false
+        self.isMultipleTouchEnabled = false
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    @discardableResult
+    public func load() -> WKNavigation? {
+        if let url = URL(string:"http://example.com") {
+            let req = URLRequest(url: url)
+            return super.load(req)
+        }
+        return nil
     }
 }
